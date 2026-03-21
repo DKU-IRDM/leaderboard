4f14808c6a6beaf8abe93c242a7e16f3896f2063
theeluwin <theeluwin@gmail.com>
2026-03-22 01:25:44 +0900

    using whitenosie

--- api/settings.py
+++ api/settings.py
@@ -47,6 +47,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 # middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -97,3 +98,4 @@ USE_TZ = True
 # static
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
